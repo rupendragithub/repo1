289c8320901067c042cb0f1bc2b2cb94f16b94ca
rupendra <rupendra.karri408@gmail.com>
2019-09-14 11:50:08 +0530

    removed the syntax errors and created exe file

--- arth.c
+++ arth.c
@@ -10,7 +10,7 @@ int main(int argc,char* argv)
 	printf("\n Multiplication : %d",a*b);
 	printf("\n substraction : %d",a-b);
 	printf("\n division : %d",a/b);
-	ptintf("\n modulo : %d",a%b);
+	printf("\n modulo : %d",a%b);
 
-	return 0
+	return 0;
 }

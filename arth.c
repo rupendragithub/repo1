2a0cb4efe2fd2f837adc1cddef7e50d40ad8cbbb
rupendra <rupendra.karri408@gmail.com>
2019-09-14 11:24:37 +0530

    added an arthemetic file

---
+++ arth.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc,char* argv)	
+{
+	
+	int a,b;
+	printf("enter values for a and b \n");
+	scanf("%d %d",&a,&b);
+	
+	printf("\n addition : %d",a+b);
+	printf("\n Multiplication : %d",a*b);
+	printf("\n substraction : %d",a-b);
+
+	return 0
+}
